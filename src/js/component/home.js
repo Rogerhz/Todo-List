cd3f88e2eaa8b7fba5748606aacd093bdf059c15
Rogerhz <rogerhernandez0260@gmail.com>
2021-04-19 16:17:10 -0400

    Todo-List

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [newTask, setNewTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 d-flex align-items-center flex-column">
+			<h1>TodoList</h1>
+			<div className="lista">
+				<input
+					className="form-control"
+					placeholder="New task"
+					value={newTask}
+					onChange={event => setNewTask(event.target.value)}
+					onKeyPress={e => {
+						if (e.key == "Enter") {
+							setTasks([
+								...tasks,
+								{
+									id: Math.random()
+										.toString(16)
+										.substring(2),
+									label: newTask
+								}
+							]);
+							setNewTask("");
+						}
+					}}
+				/>
+				<ul className="p-0">
+					{tasks.map(task => (
+						<li
+							className="list-group-item d-flex justify-content-between"
+							key={task.id}>
+							{task.label}
+							<span
+								onClick={event => {
+									let filterTasks = tasks.filter(
+										t => t.id != task.id
+									);
+									setTasks(filterTasks);
+								}}>
+								❌
+							</span>
+						</li>
+					))}
+				</ul>
+				<div>
+					<span>
+						{tasks.length == 1
+							? `${tasks.length} Remaining tasks`
+							: tasks.length == 0
+							? "Chill, no task to do"
+							: `${tasks.length} Remaining tasks`}
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 }
+document.body.style.backgroundColor = "darkgrey";
